aa689673cefae3f75be2f413fec01838e4fc3c9b
Yū <artembezzzz@gmail.com>
2024-11-16 23:29:30 +0100

    Done color unification (RGB values w/o transparency)

--- src/main/java/Utils/Print.java
+++ src/main/java/Utils/Print.java
@@ -3,11 +3,20 @@ package Utils;
 import com.diogonunes.jcolor.Ansi;
 import com.diogonunes.jcolor.Attribute;
 
+import java.awt.*;
 import java.util.Collection;
 import java.util.Optional;
 import java.util.function.Function;
 
 public class Print {
+    private static final Color info = new Color(154, 106, 221);
+    private static final Color debug = new Color(0, 34, 244);
+    private static final Color log = new Color(121, 121, 121);
+    private static final Color ioLog = new Color(65, 228, 107);
+    private static final Color plainBg = new Color(127, 127, 127);
+    private static final Color serverInfo = new Color(116, 213, 234);
+    private static final Color error = new Color(225, 85, 166);
+
     public boolean isActive;
     private String prefix;
     private boolean prefixAddition;
@@ -27,24 +36,24 @@ public class Print {
 
     public static void format(String s) {
         if (s.startsWith("<log>")) {
-            System.out.println(Ansi.colorize(s, Attribute.BRIGHT_BLACK_BACK()));
+            System.out.println(Ansi.colorize(s, getBackAttribute(log)));
         } else if (s.startsWith("<sent>") || s.startsWith("<received>")) {
             String msg = "";
             if (s.contains("<sent>")) {
-                msg += Ansi.colorize("<sent> ", Attribute.GREEN_BACK());
+                msg += Ansi.colorize("<sent> ", getBackAttribute(ioLog));
             } else {
-                msg += Ansi.colorize("<received> ", Attribute.GREEN_BACK());
+                msg += Ansi.colorize("<received> ", getBackAttribute(ioLog));
             }
-            msg += Ansi.colorize(s.substring(s.indexOf(' ')), Attribute.ITALIC(), Attribute.WHITE_BACK());
+            msg += Ansi.colorize(s.substring(s.indexOf(' ')), Attribute.ITALIC(), getBackAttribute(plainBg));
             System.out.println(msg);
         } else if (s.startsWith("<info>")) {
-            System.out.println(Ansi.colorize(s, Attribute.BOLD(), Attribute.BLUE_BACK()));
+            System.out.println(Ansi.colorize(s, Attribute.BOLD(), getBackAttribute(info)));
         } else if (s.startsWith("<debug>")) {
-            String msg = Ansi.colorize("<debug> ", Attribute.BACK_COLOR(0, 34, 244), Attribute.WHITE_TEXT());
-            msg += Ansi.colorize(s.substring(s.indexOf(' ')), Attribute.ITALIC(), Attribute.WHITE_BACK());
+            String msg = Ansi.colorize("<debug> ", getBackAttribute(debug), Attribute.WHITE_TEXT());
+            msg += Ansi.colorize(s.substring(s.indexOf(' ')), Attribute.ITALIC(), getBackAttribute(plainBg));
             System.out.println(msg);
         } else if (s.contains("<serverInfo>")) {
-            System.out.println(Ansi.colorize(s, Attribute.CYAN_BACK(), Attribute.BLACK_TEXT()));
+            System.out.println(Ansi.colorize(s, getBackAttribute(serverInfo), Attribute.BLACK_TEXT()));
         } else {
             System.out.println(s);
         }
@@ -57,34 +66,13 @@ public class Print {
                     System.out.print(prefix);
                 }
 
-                if (s.startsWith("<log>")) {
-                    System.out.println(Ansi.colorize(s, Attribute.BRIGHT_BLACK_BACK()));
-                } else if (s.startsWith("<sent>") || s.startsWith("<received>")) {
-                    String msg = "";
-                    if (s.contains("<sent>")) {
-                        msg += Ansi.colorize("<sent> ", Attribute.GREEN_BACK());
-                    } else {
-                        msg += Ansi.colorize("<received> ", Attribute.GREEN_BACK());
-                    }
-                    msg += Ansi.colorize(s.substring(s.indexOf(' ')), Attribute.ITALIC(), Attribute.WHITE_BACK());
-                    System.out.println(msg);
-                } else if (s.startsWith("<info>")) {
-                    System.out.println(Ansi.colorize(s, Attribute.BOLD(), Attribute.BLUE_BACK()));
-                } else if (s.startsWith("<debug>")) {
-                    String msg = Ansi.colorize("<debug> ", Attribute.BACK_COLOR(0, 34, 244), Attribute.WHITE_TEXT());
-                    msg += Ansi.colorize(s.substring(s.indexOf(' ')), Attribute.ITALIC(), Attribute.WHITE_BACK());
-                    System.out.println(msg);
-                } else if (s.contains("<serverInfo>")) {
-                    System.out.println(Ansi.colorize(s, Attribute.CYAN_BACK(), Attribute.BLACK_TEXT()));
-                } else {
-                    System.out.println(s);
-                }
+                format(s);
             }
         }
     }
 
     public static void error(String s) {
-        System.out.println(Ansi.colorize("<error> " + s, Attribute.BLACK_TEXT(), Attribute.RED_BACK()));
+        System.out.println(Ansi.colorize("<error> " + s, Attribute.BLACK_TEXT(), getBackAttribute(error)));
     }
 
     public void errorR(String s) {
@@ -93,7 +81,7 @@ public class Print {
                 if (prefix != null && prefixAddition) {
                     System.out.print(prefix);
                 }
-                System.out.println(Ansi.colorize("<error> " + s, Attribute.BLACK_TEXT(), Attribute.RED_BACK()));
+                error(s);
             }
         }
     }
@@ -236,5 +224,11 @@ public class Print {
         }
 
         return r;
+    }
+    private static Attribute getTextAttribute(Color color) {
+        return Attribute.TEXT_COLOR(color.getRed(), color.getGreen(), color.getBlue());
+    }
+    private static Attribute getBackAttribute(Color color) {
+        return Attribute.BACK_COLOR(color.getRed(), color.getGreen(), color.getBlue());
     }
 }
